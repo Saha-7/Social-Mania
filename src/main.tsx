abf21765473730d807653a9c2d35a179e3d948d1
saha-7op <saha7pritam@gmail.com>
2025-05-19 10:30:03 +0530

    config. added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {BrowserRouter as Router} from "react-router"
+
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
