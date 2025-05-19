16b9c11f8b8007338e2f98cfba7eae8c1f068c2a
saha-7op <saha7pritam@gmail.com>
2025-05-19 10:46:06 +0530

    Home pge setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css'
+import { Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Navbar/>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
